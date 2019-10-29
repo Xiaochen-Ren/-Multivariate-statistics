85a6b727fa65e7c9c48e86a9e59a5eb0f3482e76
Xiaochen-Ren <53121181+Xiaochen-Ren@users.noreply.github.com>
2019-10-29 11:08:35 +0800

    Update Clustering_method.R

--- Clustering_method.R
+++ Clustering_method.R
@@ -1,15 +1,16 @@
 x=iris
 target=x[,5:5]
-iris_train_pca <- princomp(x[,1:4])
+iris_train_pca <- princomp(x[,1:4])#主成分分析降维
 new_data <- predict(iris_train_pca, x[,-5])[,1:2]
-set.seed(20)
-cl<-kmeans(new_data,3)
+set.seed(20)#设置随机种子
+cl<-kmeans(new_data,3)#kmeans聚类
+#绘图
 split.screen(c(2,1))
 screen(1)
 p1<-plot(new_data,col=cl$ cluster,sub="k-means聚类")
 screen(2)
 p2<-plot(new_data,col=target,sub="标签")
-
+#计算准确率
 num_true<-0
 for(i in 1:length(target))
 {
