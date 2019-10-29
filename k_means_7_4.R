75c4046c40c40af7c948377af5e6f8d23e1b46c7
Xiaochen-Ren <53121181+Xiaochen-Ren@users.noreply.github.com>
2019-10-29 11:06:48 +0800

    Update k_means_7_4.R

--- k_means_7_4.R
+++ k_means_7_4.R
@@ -1,7 +1,8 @@
-x1=matrix(rnorm(100,mean=0,sd=0.3),ncol=2)
-x2=matrix(rnorm(100,mean=1,sd=0.3),ncol=2)
-x=rbind(x1,x2)
-cl<-kmeans(x,2)
+x1=matrix(rnorm(100,mean=0,sd=0.3),ncol=2)#生成正态数据
+x2=matrix(rnorm(100,mean=1,sd=0.3),ncol=2)#生成正态数据
+x=rbind(x1,x2)#拼接数据
+cl<-kmeans(x,2)#k均值聚类
+#绘图
 pch1=rep("1",10000)
 pch2=rep("2",10000)
 plot(x,col=cl$ cluster,pch=c(pch1,pch2))
